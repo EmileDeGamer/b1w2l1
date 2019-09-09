a78978277b83d9031ad62af076c9f86787dd7722
EmileDeGamer <emilemolzoho@gmail.com>
2019-09-09 12:39:09 +0200

    Changes

---
+++ b1w2l1.js
@@ -0,0 +1,9 @@
+//Emile Mol
+//Applicatie en Media Ontwikkelaar
+
+alert('U moet drie vragen beantwoorden') //Toont pop-up
+let voornaam = prompt('Wat is je voornaam?', 'Voornaam...') //Voornaam vraag
+let achternaam = prompt('Wat is je achternaam?', 'Achternaam...') //Achternaam vraag
+let leeftijd = prompt('Wat is je leeftijd', 'Leeftijd...') //Leeftijd vraag
+
+document.write(voornaam + " " + achternaam + " " + leeftijd) //Laat alles zien in een regel
